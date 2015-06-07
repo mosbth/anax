fa28d90360861df6433cb0e82df5bc5310ab97dc
Mikael Roos <mikael.t.h.roos@gmail.com>
2015-06-07 22:50:58 +0200

    removing include of functions from test#

--- test/config.php
+++ test/config.php
@@ -11,21 +11,12 @@
  *
  */
 define('ANAX_INSTALL_PATH', realpath(__DIR__ . '/../') . '/');
-define('ANAX_APP_PATH',     ANAX_INSTALL_PATH . 'app/');
+define('ANAX_APP_PATH', ANAX_INSTALL_PATH . 'app/');
 
 
 
 /**
  * Include autoloader.
- * 
- */
-include(ANAX_APP_PATH . 'config/autoloader.php'); 
-
-
-
-/**
- * Include global functions.
  *
  */
-include(ANAX_INSTALL_PATH . 'src/functions.php'); 
-
+include(ANAX_APP_PATH . 'config/autoloader.php');
