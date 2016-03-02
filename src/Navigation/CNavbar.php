390ac4d5d57bbae2fd8cb5d4c2056c8e7f778e68
Mikael Roos <mikael.t.h.roos@gmail.com>
2016-03-02 17:41:41 +0100

    navbar to responsive

--- src/Navigation/CNavbar.php
+++ src/Navigation/CNavbar.php
@@ -34,10 +34,15 @@ class CNavbar
         );
         $menu = array_replace_recursive($default, $this->config);
 
-        // Create the ul li menu from the array, use an anonomous recursive function that returns an
-        // array of values.
-        $createMenu = function ($items, $callback) use (&$createMenu, $menu) {
-            
+        // Create the ul li menu from the array, use an anonomous recursive
+        // function that returns an array of values.
+        $createMenu = function (
+            $items,
+            $callback,
+            $ulId = null,
+            $ulClass = null
+        ) use (&$createMenu, $menu) {
+
             $html = null;
             $hasItemIsSelected = false;
 
@@ -45,7 +50,7 @@ class CNavbar
                 // has submenu, call recursivly and keep track on if the submenu has a selected item in it.
                 $submenu        = null;
                 $selectedParent = null;
-            
+
                 if (isset($item['submenu'])) {
                     list($submenu, $selectedParent) = $createMenu($item['submenu']['items'], $callback);
                     $selectedParent = $selectedParent
@@ -57,7 +62,7 @@ class CNavbar
                 $selected = $callback($item['url'])
                     ? "selected "
                     : null;
-                
+
                 // Check if the menuitem is a parent of current page, /controller for /controller/action
                 $isParent = null;
                 if (isset($item['mark-if-parent-of']) && $item['mark-if-parent-of'] == true) {
@@ -65,7 +70,7 @@ class CNavbar
                         ? "is-parent "
                         : null;
                 }
-                
+
                 // Is there a class set for this item, then use it
                 $class = isset($item['class']) && ! is_null($item['class'])
                     ? $item['class']
@@ -79,7 +84,7 @@ class CNavbar
                 // Add the menu item
                 $url = $menu['create_url']($item['url']);
                 $html .= "\n<li{$class}><a href='{$url}' title='{$item['title']}'>{$item['text']}</a>{$submenu}</li>\n";
-                
+
                 // To remember there is selected children when going up the menu hierarchy
                 if ($selected) {
                     $hasItemIsSelected = true;
@@ -87,18 +92,30 @@ class CNavbar
             }
 
             // Return the menu
-            return array("\n<ul>$html</ul>\n", $hasItemIsSelected);
+            return array("\n<ul$ulId$ulClass>$html</ul>\n", $hasItemIsSelected);
         };
 
         // Call the anonomous function to create the menu, and submenues if any.
-        list($html) = $createMenu($menu['items'], $menu['callback']);
+        $id = isset($menu['id'])
+            ? " id=\"{$menu['id']}\""
+            : null;
+        $class = isset($menu['class'])
+            ? " class=\"{$menu['class']}\""
+            : null;
 
+        list($html) = $createMenu(
+            $menu['items'],
+            $menu['callback'],
+            $id,
+            $class
+        );
 
         // Set the id & class element, only if it exists in the menu-array
-        $id      = isset($menu['id'])    ? " id='{$menu['id']}'"       : null;
-        $class   = isset($menu['class']) ? " class='{$menu['class']}'" : null;
         $wrapper = $menu['wrapper'];
+        if ($wrapper) {
+            $html = "<{$wrapper}>{$html}</{$wrapper}>";
+        }
 
-        return "\n<{$wrapper}{$id}{$class}>{$html}</{$wrapper}>\n";
+        return "\n{$html}\n";
     }
 }
--- view/default/navbar.tpl.php
+++ view/default/navbar.tpl.php
@@ -1,1 +1,9 @@
-<?= $this->di->get("navbar")->create() ?>
+<div class="rm-navbar">
+
+    <ul class="rm-small">
+        <li><a id="rm-menu-button" class="rm-button" href="#"><i class="fa fa-bars fa-2x"></i></a></li>
+    </ul>
+
+    <?= $this->di->get("navbar")->create() ?>
+
+</div>
