2c191414d10db6cd1cd69b5ae8f9fcfc99f212da
Mikael Roos <mikael.t.h.roos@gmail.com>
2016-03-07 17:55:20 +0100

    merged with anax-flat

--- webroot/index_template.php
+++ webroot/index_template.php
@@ -1,18 +1,25 @@
 <?php
+// Where did we install anax-flat? (without ending slash)
+define("ANAX_INSTALL_PATH", realpath(__DIR__ . "/.."));
 
-define('ANAX_INSTALL_PATH', realpath(__DIR__ . '/..'));
-define('ANAX_APP_PATH', ANAX_INSTALL_PATH . '/app');
+// Where did we install anax? (without ending slash)
+define("ANAX_APP_PATH", ANAX_INSTALL_PATH . "/app");
 
-require ANAX_INSTALL_PATH . '/vendor/autoload.php';
+// We need the autoloader to start with
+require ANAX_INSTALL_PATH . "/vendor/autoload.php";
 
+// Lets create a $di service container and inject it to our $app
 $di  = new \Anax\DI\CDIFactoryDefault();
 $app = new \Anax\Kernel\CAnax($di);
 
+// The routes are defined in a separate file
 $routes = ANAX_INSTALL_PATH . "/config/routes.php";
 if (is_file(ANAX_APP_PATH . "/config/routes.php")) {
     $routes = ANAX_APP_PATH . "/config/routes.php";
 }
 require $routes;
 
+// Lets check if the current request matches a route and if so
+// render a response.
 $app->router->handle();
 $app->theme->render();
