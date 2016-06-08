6c31dfeb2b192a545127aa59b30e5c511dbdf76d
Mikael Roos <mikael.t.h.roos@gmail.com>
2016-06-08 10:55:03 +0200

    ...

--- view/default/navbar.tpl.php
+++ view/default/navbar.tpl.php
@@ -1,8 +1,10 @@
 <div class="rm-navbar">
 
-    <ul class="rm-small">
-        <li><a id="rm-menu-button" class="rm-button" href="#"><i class="fa fa-bars fa-2x"></i></a></li>
-    </ul>
+    <div class="rm-small-wrapper">
+        <ul class="rm-small">
+            <li><a id="rm-menu-button" class="rm-button" href="#"><i class="fa fa-bars fa-2x"></i></a></li>
+        </ul>
+    </div>
 
     <?= $this->di->get("navbar")->create() ?>
 
